2ee85b9b52e1ffe66f9f9843cbc79ba8ab314ae1
Pratikesh <pratikeshb9@gmail.com>
2026-07-04 00:20:12 +0530

    Add the basic project structure, Install Tailwind and shadcn/ui for better UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <h1>hey</h1>
-      </div>
-      
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Doctor Patient Management System
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
